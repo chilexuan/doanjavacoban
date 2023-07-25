77215e29d9dadb54989225eead860707537b7887
chilexuan <chilexuan333@gmail.com>
2023-07-25 19:27:57 +0700

    thay doi lan 3

--- src/DoAnJaVa/DoAnJaVaCoBan.java
+++ src/DoAnJaVa/DoAnJaVaCoBan.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.util.Scanner;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class DoAnJaVaCoBan {
@@ -19,6 +17,7 @@ public class DoAnJaVaCoBan {
 		double[] dVan = new double[n];
 		double[] dAnh = new double[n];
 		double[] dTB = new double[n];
+		String[] xepLoai = xepLoaiHocLuc(dTB);
 
 		char key;
 		do {
@@ -32,36 +31,39 @@ public class DoAnJaVaCoBan {
 				nhapHoTenVaDiem(hoten, dToan, dVan, dAnh);
 				break;
 			case '2':
-
 				dTB = tinhDTB(dToan, dVan, dAnh);
-				inDSSV(hoten, dToan, dVan, dAnh, dTB);
+				inDSSV(hoten, dToan, dVan, dAnh, dTB, xepLoai);
 				break;
 			case '3':
-				inDSSV(hoten, dToan, dVan, dAnh, dTB);
+				xepLoai = xepLoaiHocLuc(dTB);
+				inDSSV(hoten, dToan, dVan, dAnh, dTB, xepLoai);
 				break;
 			case '4':
-				sapxepDSSV(hoten, dToan, dVan, dAnh, dTB);
-				inDSSV(hoten, dToan, dVan, dAnh, dTB);
+				inDSSV(hoten, dToan, dVan, dAnh, dTB, xepLoai);
 				break;
 			case '5':
-				timkiemDiem(hoten, dToan, dVan, dAnh, dTB);
-				break;
-			case '6':
-				timHoTen(hoten, dToan, dVan, dAnh, dTB);
-				break;
-			case '7':
-				nhapFile(hoten, dToan, dVan, dAnh, dTB);
-				break;
-			case '8':
-				xuatFile(hoten, dToan, dVan, dAnh, dTB);
-				break;
-			case '9':
-				timSVCoDTBCaoNhat(hoten, dToan, dVan, dAnh, dTB);
-				break;
-			case 'A':
 				suaDuLieuSinhVien(hoten, dToan, dVan, dAnh, dTB);
 				break;
-			case 'Q':
+			case '6':
+				sapxepDSSV(hoten, dToan, dVan, dAnh, dTB, xepLoai);
+				inDSSV(hoten, dToan, dVan, dAnh, dTB, xepLoai);
+				break;
+			case '7':
+				timkiemDiem(hoten, dToan, dVan, dAnh, dTB, xepLoai);
+				break;
+			case '8':
+				timHoTen(hoten, dToan, dVan, dAnh, dTB, xepLoai);
+				break;
+			case '9':
+				timSVCoDTBCaoNhat(hoten, dToan, dVan, dAnh, dTB, xepLoai);
+				break;
+			case 'x':
+				xuatFile(hoten, dToan, dVan, dAnh, dTB, xepLoai);
+				break;
+			case 'z':
+				nhapFile(hoten, dToan, dVan, dAnh, dTB, xepLoai);
+				break;
+			case'Q':
 				System.out.println("Đã thoát chương trình");
 				System.exit(0);
 
@@ -79,15 +81,15 @@ public class DoAnJaVaCoBan {
 		System.out.println("*****************************************************");
 		System.out.println("(1): NHẬP DỮ LIỆU SINH VIÊN");
 		System.out.println("(2): TÍNH ĐIỂM TRUNG BÌNH");
-		System.out.println("(3): IN DANH SÁCH SINH VIÊN");
-		System.out.println("(4): SẮP XẾP SINH VIÊN THEO ĐIỂM TRUNG BÌNH");
-		System.out.println("(5): TÌM KIẾM THEO ĐIỂM TOÁN");
-		System.out.println("(6): TÌM SINH VIÊN THEO HỌ VÀ TÊN");
-		System.out.println("(7): GHI FILE VĂN BẢN");
-		System.out.println("(8): ĐỌC FILE VĂN BẢN");
+		System.out.println("(3): XẾP LOẠI HỌC LỰC SINH VIÊN");
+		System.out.println("(4): IN DANH SÁCH SINH VIÊN");
+		System.out.println("(5): SỬA DỮ LIỆU SINH VIÊN");
+		System.out.println("(6): SẮP XẾP SINH VIÊN THEO ĐIỂM TRUNG BÌNH");
+		System.out.println("(7): TÌM KIẾM THEO ĐIỂM TOÁN");
+		System.out.println("(8): TÌM SINH VIÊN THEO HỌ VÀ TÊN");
 		System.out.println("(9): TÌM SINH VIÊN CÓ ĐIỂM TRUNG BÌNH CAO NHẤT");
-		System.out.println("(A): SỬA DỮ LIỆU SINH VIÊN");
-		System.out.println("(Q/q): THOÁT KHỎI CHƯƠNG TRÌNH");
+		System.out.println("(x): XUẤT FILE VĂN BẢN");
+		System.out.println("(z): NHẬP FILE VĂN BẢN");	
 		System.out.println("*******************************************************");
 	}
 
@@ -208,8 +210,31 @@ public class DoAnJaVaCoBan {
 		return dtb;
 	}
 
+	// Hàm xếp loại học lực
+	public static String[] xepLoaiHocLuc(double[] dTB) {
+		String[] xepLoai = new String[dTB.length];
+		for (int i = 0; i < dTB.length; i++) {
+			if (dTB[i] >= 9.0) {
+				xepLoai[i] = "Xuất sắc";
+			} else if (dTB[i] >= 8.0) {
+				xepLoai[i] = "Giỏi";
+			} else if (dTB[i] >= 6.5) {
+				xepLoai[i] = "Khá";
+			} else if (dTB[i] >= 5.0) {
+				xepLoai[i] = "Trung bình ";
+			} else if (dTB[i] >= 3.5) {
+				xepLoai[i] = "Yếu";
+			} else {
+				xepLoai[i] = "Kém";
+			}
+		}
+		return xepLoai;
+	}
+
+
+
 // Ham sap xep sinh vien theo chieu giam dan cua diem trung binh
-	public static void sapxepDSSV(String[] hoten, double[] dToan, double[] dVan, double[] dAnh, double[] dTB) {
+	public static void sapxepDSSV(String[] hoten, double[] dToan, double[] dVan, double[] dAnh, double[] dTB,String[] xepLoai) {
 		for (int i = 0; i < dTB.length - 1; i++) {
 			for (int j = i + 1; j < dTB.length; j++) {
 				if (dTB[i] < dTB[j]) {
@@ -233,6 +258,9 @@ public class DoAnJaVaCoBan {
 					String tam2 = hoten[i];
 					hoten[i] = hoten[j];
 					hoten[j] = tam2;
+					 tam2 = xepLoai[i];
+		                xepLoai[i] = xepLoai[j];
+		                xepLoai[j] = tam2;
 				}
 			}
 
@@ -240,23 +268,26 @@ public class DoAnJaVaCoBan {
 	}
 
 // Ham in danh sach sinh vien ra man hinh
-	public static void inDSSV(String[] hoten, double[] dToan, double[] dVan, double[] dAnh, double[] dTB) {
+	public static void inDSSV(String[] hoten, double[] dToan, double[] dVan, double[] dAnh, double[] dTB,
+			String[] xepLoai) {
 		System.out.println("*************************************************************");
 		System.out.println("\t\t\t DANH SÁCH SINH VIÊN");
-		System.out.println("+---------------------+-----------+-----------+-----------+-----------+");
-		System.out.printf("| %-20s| %-10s| %-10s| %-10s| %-10s|%n", "Họ và tên", "Toán", "Văn", "Anh", "Điểm TB");
-		System.out.println("+---------------------+-----------+-----------+-----------+-----------+");
+		System.out.println("+---------------------+-----------+-----------+-----------+-----------+----------------+");
+		System.out.printf("| %-20s| %-10s| %-10s| %-10s| %-10s| %-15s|%n", "Họ và tên", "Toán", "Văn", "Anh", "Điểm TB",
+				"Xếp loại");
+		System.out.println("+---------------------+-----------+-----------+-----------+-----------+----------------+");
 
 		for (int i = 0; i < hoten.length; i++) {
-			System.out.printf("| %-20s| %-10.2f| %-10.2f| %-10.2f| %-10.2f|%n", hoten[i], dToan[i], dVan[i], dAnh[i],
-					dTB[i]);
+			System.out.printf("| %-20s| %-10.2f| %-10.2f| %-10.2f| %-10.2f| %-15s|%n", hoten[i], dToan[i], dVan[i],
+					dAnh[i], dTB[i], xepLoai[i]);
 		}
 
 		System.out.println("*************************************************************");
 	}
 
 // Ham tim kiem sinh vien theo diem mon Toan
-	public static void timkiemDiem(String[] hoten, double[] dToan, double[] dVan, double[] dAnh, double[] dTB) {
+	public static void timkiemDiem(String[] hoten, double[] dToan, double[] dVan, double[] dAnh, double[] dTB,
+			String[] xepLoai) {
 		int count = 0;
 		Scanner sc = new Scanner(System.in);
 		double diemcantim;
@@ -268,15 +299,16 @@ public class DoAnJaVaCoBan {
 				for (int i = 0; i < dToan.length; i++) {
 					if (dToan[i] == diemcantim) {
 						if (count == 0) {
-							System.out
-									.println("+---------------------+-----------+-----------+-----------+-----------+");
-							System.out.printf("| %-20s| %-10s| %-10s| %-10s| %-10s|%n", "Họ và tên", "Toán", "Văn",
-									"Anh", "Điểm TB");
-							System.out
-									.println("+---------------------+-----------+-----------+-----------+-----------+");
+							System.out.println("\t\t\t DANH SÁCH SINH VIÊN");
+							System.out.println(
+									"+---------------------+-----------+-----------+-----------+-----------+----------------+");
+							System.out.printf("| %-20s| %-10s| %-10s| %-10s| %-10s| %-15s|%n", "Họ và tên", "Toán",
+									"Văn", "Anh", "Điểm TB", "Xếp loại");
+							System.out.println(
+									"+---------------------+-----------+-----------+-----------+-----------+----------------+");
 						}
-						System.out.printf("| %-20s| %-10.2f| %-10.2f| %-10.2f| %-10.2f|%n", hoten[i], dToan[i], dVan[i],
-								dAnh[i], dTB[i]);
+						System.out.printf("| %-20s| %-10.2f| %-10.2f| %-10.2f| %-10.2f| %-15s|%n", hoten[i], dToan[i], dVan[i],
+								dAnh[i], dTB[i], xepLoai[i]);
 						count = count + 1;
 					}
 				}
@@ -290,7 +322,7 @@ public class DoAnJaVaCoBan {
 	}
 
 // Ham tim kiem sinh vien theo ho ten
-	public static void timHoTen(String[] hoten, double[] dToan, double[] dVan, double[] dAnh, double[] dTB) {
+	public static void timHoTen(String[] hoten, double[] dToan, double[] dVan, double[] dAnh, double[] dTB,String[] xepLoai) {
 		int count = 0;
 		Scanner sc = new Scanner(System.in);
 		String x;
@@ -301,15 +333,15 @@ public class DoAnJaVaCoBan {
 				for (int i = 0; i < hoten.length; i++) {
 					if (hoten[i].equals(x)) {
 						if (count == 0) {
-							System.out
-									.println("+---------------------+-----------+-----------+-----------+-----------+");
-							System.out.printf("| %-20s| %-10s| %-10s| %-10s| %-10s|%n", "Họ và tên", "Toán", "Văn",
-									"Anh", "Điểm TB");
-							System.out
-									.println("+---------------------+-----------+-----------+-----------+-----------+");
+							System.out.println(
+									"+---------------------+-----------+-----------+-----------+-----------+----------------+");
+							System.out.printf("| %-20s| %-10s| %-10s| %-10s| %-10s| %-15s|%n", "Họ và tên", "Toán",
+									"Văn", "Anh", "Điểm TB", "Xếp loại");
+							System.out.println(
+									"+---------------------+-----------+-----------+-----------+-----------+----------------+");
 						}
-						System.out.printf("| %-20s| %-10.2f| %-10.2f| %-10.2f| %-10.2f|%n", hoten[i], dToan[i], dVan[i],
-								dAnh[i], dTB[i]);
+						System.out.printf("| %-20s| %-10.2f| %-10.2f| %-10.2f| %-10.2f| %-15s|%n", hoten[i], dToan[i], dVan[i],
+								dAnh[i], dTB[i], xepLoai[i]);
 						count = count + 1;
 					}
 				}
@@ -323,42 +355,40 @@ public class DoAnJaVaCoBan {
 
 	}
 
-	public static void timSVCoDTBCaoNhat(String[] hoten, double[] dToan, double[] dVan, double[] dAnh, double[] dTB) {
-		double max = dTB[0];
-		for (int i = 1; i < dTB.length; i++) {
-			if (dTB[i] > max) {
-				max = dTB[i];
-			}
-		}
-		System.out.println("Sinh viên có điểm trung bình cao nhất: ");
-		for (int i = 0; i < dTB.length; i++) {
-			if (dTB[i] == max) {
-				System.out.println("+---------------------+-----------+-----------+-----------+-----------+");
-				System.out.printf("| %-20s| %-10s| %-10s| %-10s| %-10s|%n", "Họ và tên", "Toán", "Văn", "Anh",
-						"Điểm TB");
-				System.out.println("+---------------------+-----------+-----------+-----------+-----------+");
+	public static void timSVCoDTBCaoNhat(String[] hoten, double[] dToan, double[] dVan, double[] dAnh, double[] dTB, String[] xepLoai) {
+	    double max = dTB[0];
+	    for (int i = 1; i < dTB.length; i++) {
+	        if (dTB[i] > max) {
+	            max = dTB[i];
+	        }
+	    }
+	    System.out.println("Sinh viên có điểm trung bình cao nhất: ");
+	    System.out.println("+---------------------+-----------+-----------+-----------+-----------+----------------+");
+	    System.out.printf("| %-20s| %-10s| %-10s| %-10s| %-10s| %-15s|%n", "Họ và tên", "Toán", "Văn", "Anh", "Điểm TB", "Xếp loại");
+	    System.out.println("+---------------------+-----------+-----------+-----------+-----------+----------------+");
+	    for (int i = 0; i < dTB.length; i++) {
+	        if (dTB[i] == max) {
+	            System.out.printf("| %-20s| %-10.2f| %-10.2f| %-10.2f| %-10.2f| %-15s|%n", hoten[i], dToan[i], dVan[i], dAnh[i], dTB[i], xepLoai[i]);
+	        }
+	    }
+	}
+	
 
-				System.out.printf("| %-20s| %-10.2f| %-10.2f| %-10.2f| %-10.2f|%n", hoten[i], dToan[i], dVan[i],
-						dAnh[i], dTB[i]);
-			}
-		}
+	public static void xuatFile(String[] hoten, double[] dToan, double[] dVan, double[] dAnh, double[] dTB, String[] xepLoai) {
+	    try {
+	        FileWriter fw = new FileWriter("danhsachsinhvien.txt");
+	        for (int i = 0; i < hoten.length; i++) {
+	            fw.write(hoten[i] + " - Điểm Toán: " + dToan[i] + " - Điểm Văn: " + dVan[i] + " - Điểm Anh: " + dAnh[i]
+	                    + " - Điểm trung bình: " + dTB[i] +" - Xếp loại: " + xepLoai[i] + "\n");
+	        }
+	        fw.close();
+	        System.out.println("Đã xuất file thành công!");
+	    } catch (IOException e) {
+	        System.out.println("Đã sảy ra lỗi trong quá trình xuất file!");
+	    }
 	}
 
-	public static void xuatFile(String[] hoten, double[] dToan, double[] dVan, double[] dAnh, double[] dTB) {
-		try {
-			FileWriter fw = new FileWriter("danhsachsinhvien.txt");
-			for (int i = 0; i < hoten.length; i++) {
-				fw.write(hoten[i] + " - Điểm Toán: " + dToan[i] + " - Điểm Văn: " + dVan[i] + " - Điểm Anh: " + dAnh[i]
-						+ " - Điểm trung bình: " + dTB[i] + "\n");
-			}
-			fw.close();
-			System.out.println("Đã xuất file thành công!");
-		} catch (IOException e) {
-			System.out.println("Đã sảy ra lỗi trong quá trình xuất file!");
-		}
-	}
-
-	public static void nhapFile(String[] hoten, double[] dToan, double[] dVan, double[] dAnh, double[] dTB) {
+	public static void nhapFile(String[] hoten, double[] dToan, double[] dVan, double[] dAnh, double[] dTB,String[] xepLoai) {
 		try {
 			BufferedReader br = new BufferedReader(new FileReader("danhsachsinhvien.txt"));
 			String line;
@@ -370,6 +400,7 @@ public class DoAnJaVaCoBan {
 				dVan[i] = Double.parseDouble(data[2].split(": ")[1]);
 				dAnh[i] = Double.parseDouble(data[3].split(": ")[1]);
 				dTB[i] = Double.parseDouble(data[4].split(": ")[1]);
+				 xepLoai[i] = data[5].split(": ")[1];
 				i++;
 			}
 			br.close();
